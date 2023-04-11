5899e9b0674615019776d6493f0867d6d1b76716
Tel <tel@jsdfyodds.com>
2023-04-11 17:14:59 -0400

    fix h1 pdf text

--- wp-content/themes/divi-child-theme/scripts.js
+++ wp-content/themes/divi-child-theme/scripts.js
@@ -134,6 +134,7 @@ jQuery(document).ready(function ($)
                         console.log(image_links);
                         links.h1 = {
                             fontFamily: "Arvo",
+                            fontSize: 48,
                             textColor: "#000000",
                             alignment: "center",
                             fontWeight: "bold",
