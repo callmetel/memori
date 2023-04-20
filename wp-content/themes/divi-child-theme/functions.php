579ac01ec87140dccf5e710616876acb9655c39a
Tel <tel@jsdfyodds.com>
2023-04-20 17:23:36 -0400

    add php to correct image orientation

--- wp-content/themes/divi-child-theme/functions.php
+++ wp-content/themes/divi-child-theme/functions.php
@@ -191,6 +191,36 @@ function reArrayFiles(&$file_post)
 	return $file_ary;
 }
 
+function correctImageOrientation($filename)
+{
+	if (function_exists('exif_read_data')) {
+		$exif = exif_read_data($filename);
+		if ($exif && isset($exif['Orientation'])) {
+			$orientation = $exif['Orientation'];
+			if ($orientation != 1) {
+				$img = imagecreatefromjpeg($filename);
+				$deg = 0;
+				switch ($orientation) {
+					case 3:
+						$deg = 180;
+						break;
+					case 6:
+						$deg = 270;
+						break;
+					case 8:
+						$deg = 90;
+						break;
+				}
+				if ($deg) {
+					$img = imagerotate($img, $deg, 0);
+				}
+				// then rewrite the rotated image back to the disk as $filename 
+				imagejpeg($img, $filename, 95);
+			} // if there is some rotation necessary
+		} // if have the exif orientation info
+	} // if function exists      
+}
+
 // Upload Images To Media Folder
 function add_tmpimgs()
 {
@@ -215,6 +245,9 @@ function add_tmpimgs()
 				// New image location
 				ABSPATH . DIRECTORY_SEPARATOR . $image_location
 			);
+
+			// Correct image orientation
+			correctImageOrientation(ABSPATH . DIRECTORY_SEPARATOR . $image_location);
 
 			// Add image link to image_links array
 			$image_url = get_base_url() . DIRECTORY_SEPARATOR . $image_location;
